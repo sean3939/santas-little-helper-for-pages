a302b1f621be183aecb4a1bf6bc89cee9b200a9c
Sean Solomon <sean3939@gmail.com>
2025-12-18 13:33:15 -0600

    fix for api key type and jszip per claude.

--- src/services/exportService.ts
+++ src/services/exportService.ts
@@ -1,5 +1,3 @@
-import JSZip from 'jszip';
-
 // This is the file you are looking at!
 // Since we don't want infinite recursion in the zip, we won't include the giant data blob here.
 export const downloadRepo = async () => {
